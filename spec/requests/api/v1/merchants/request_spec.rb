d52225c7783858b441381d326598ce082b68c24f
Candace Eckels <candace.eckels@gmail.com>
2022-10-18 22:37:00 -0600

    Merchants endpoint

---
+++ spec/requests/api/v1/merchants/request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Items API" do
+  
+  describe 'sends a list of merchants' do
+    it 'sends merchant info' do
+      
+    end
+
+    it 'sends an array even if blank'
+  end
+end
