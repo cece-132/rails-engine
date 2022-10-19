d88f035f6e4b74232cc68635235551922081d289
Candace Eckels <candace.eckels@gmail.com>
2022-10-19 17:40:37 -0600

    Delete almost done

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,19 @@
 class Item < ApplicationRecord
+  enum status: ["Enabled", "Disabled"]
   belongs_to :merchant
-  has_many :invoice_items
-  has_many :invoices, through: :invoice_items
+  has_many :invoice_items, :dependent => :destroy
+  has_many :invoices, through: :invoice_items, counter_cache: true
+  has_many :items, through: :invoice_items, counter_cache: true
+  # after_destroy :cleanup
+  
 
   validates_presence_of :name
   validates_presence_of :description
   validates_presence_of :unit_price
   validates_presence_of :merchant_id
+
+  private
+  # def cleanup
+  #   Invoice.cleanup
+  # end
 end
--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
   factory :invoice_item do
     quantity { rand(150) }
     unit_price { rand(50..20000)}
-    status { valid_status.sample }
+    # status { valid_status.sample }
     invoice
     item
   end
