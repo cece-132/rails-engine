1c9d9208e9dd2e5355834b3319da67231219bf4a
Candace Eckels <candace.eckels@gmail.com>
2022-10-19 09:50:34 -0600

    can get a single item/merchant

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -12,4 +12,15 @@ class MerchantSerializer
       }  
   end
 
+  def self.format_merchant(merchant)
+    { data: 
+      { id: "#{merchant.id}",
+       type: merchant.class.to_s.downcase,
+         attributes: { 
+           name: merchant.name
+         }
+       }
+     }  
+  end
+
 end
--- spec/requests/api/v1/merchants/request_spec.rb
+++ spec/requests/api/v1/merchants/request_spec.rb
@@ -45,4 +45,32 @@ RSpec.describe "Items API" do
       expect(merchants[:data].count).to eq 0
     end
   end
+
+  describe 'send a single merchant' do
+    it 'sends the merchant w/ id' do
+      merchant = create(:merchant)
+
+      get "/api/v1/merchants/#{merchant.id}"
+
+      expect(response).to be_successful
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchant).to be_a(Hash)
+      expect(merchant).to have_key(:data)
+      expect(merchant[:data]).to be_a Hash
+
+      expect(merchant[:data]).to have_key(:id)
+      expect(merchant[:data]).to have_key(:attributes)
+      
+      expect(merchant[:data][:id]).to be_a(String)
+      expect(merchant[:data][:attributes]).to be_a(Hash)
+
+      expect(merchant[:data][:attributes]).to have_key(:name)
+      expect(merchant[:data][:attributes][:name]).to be_a(String)
+    end
+
+    it 'sad path, bad integer id returns 404'
+    it 'edge case, string id returns 404'
+  end
 end
