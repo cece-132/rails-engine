8a7f3df6269f28184a5abe8aee080d95bb051903
Candace Eckels <candace.eckels@gmail.com>
2022-10-20 13:25:37 -0600

    returns merchants items

---
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::MerchantItemsController < ApplicationController
+
+  def index
+    merchant = Merchant.find(params[:merchant_id])
+    render json: ItemSerializer.format_items(merchant.items)
+  end
+
+end
