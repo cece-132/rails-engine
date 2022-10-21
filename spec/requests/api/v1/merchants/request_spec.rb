b8330889ebbad6386636d72d364bc066d3ca680b
Candace Eckels <candace.eckels@gmail.com>
2022-10-21 15:28:03 -0600

    Merchants Index test passing

---
+++ spec/requests/api/v1/merchants/request_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "Merchant API" do
+  describe 'index' do
+    it 'retuns all merchants' do
+      create_list(:merchant, 10)
+
+      get "/api/v1/merchants"
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants).to be_a Hash
+      expect(merchants).to have_key(:data)
+      expect(merchants[:data]).to be_a Array
+
+      merchants[:data].each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_a(String)
+
+        expect(merchant).to have_key(:type)
+        expect(merchant[:type]).to be_a(String)
+
+        expect(merchant).to have_key(:attributes)
+        expect(merchant[:attributes]).to be_a(Hash)
+
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a(String)
+      end
+
+      
+    end
+  end
+end
