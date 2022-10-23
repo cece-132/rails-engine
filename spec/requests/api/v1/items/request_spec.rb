05b36570fde709cb86a4742f8e606619bb58e1d9
Candace Eckels <candace.eckels@gmail.com>
2022-10-23 13:05:59 -0600

    Items show complete

--- spec/requests/api/v1/items/request_spec.rb
+++ spec/requests/api/v1/items/request_spec.rb
@@ -41,6 +41,28 @@ RSpec.describe "Items API" do
       end
 
     end
+  end
 
+  describe 'show' do
+    it 'returns 1 item' do
+      merchant = create(:merchant)
+      item_1 = create(:item, merchant_id: merchant.id)
+
+      get "/api/v1/items/#{item_1.id}"
+
+      expect(response).to be_successful
+
+      item = JSON.parse(response.body, symbolize_names: true)
+
+      expect(item).to be_a Hash
+      expect(item).to have_key(:data)
+      expect(item[:data]).to have_key(:id)
+      expect(item[:data]).to have_key(:type)
+      expect(item[:data]).to have_key(:attributes)
+      expect(item[:data][:attributes]).to have_key(:name)
+      expect(item[:data][:attributes]).to have_key(:description)
+      expect(item[:data][:attributes]).to have_key(:unit_price)
+      expect(item[:data][:attributes]).to have_key(:merchant_id)
+    end
   end
 end
