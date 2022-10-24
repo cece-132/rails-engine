0a9528f205a3ddea713c5b01e4af0f08f9c9b088
Candace Eckels <candace.eckels@gmail.com>
2022-10-23 21:00:22 -0600

    Merchant find_all

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -2,21 +2,34 @@ class Api::V1::Merchants::SearchController < ApplicationController
 
   def find
     merchants = Merchant.where(nil)
-    @merchant = merchants.filter_name(params[:name])
-    if @merchant.present?
-      render json: MerchantSerializer.new(@merchant.first)
+    if params[:name].present?
+      if !params[:name].empty?
+        @merchant = merchants.filter_name(params[:name])
+        if @merchant.present?
+          render json: MerchantSerializer.new(@merchant.first)
+        else
+          render json: { data: {}, error: 'error' }, status: 400
+        end
+      else
+        render json: { data: {}, error: 'error' }, status: 400
+      end
     else
       render json: { data: {}, error: 'error' }, status: 400
     end
+
   end
 
   def find_all
     merchants = Merchant.where(nil)
-    @merchants = merchants.filter_name(params[:name])
-    if @merchants.present?
-      render json: MerchantSerializer.new(@merchants)
+    if !params[:name].empty?
+      @merchants = merchants.filter_name(params[:name])
+      if @merchants.present?
+        render json: MerchantSerializer.new(@merchants)
+      else
+        render json: { data: [], error: 'error' }, status: 400
+      end
     else
-      render json: { data: [], error: 'error' }, status: 400
+      render json: { data: {}, error: 'error' }, status: 400
     end
   end
   
